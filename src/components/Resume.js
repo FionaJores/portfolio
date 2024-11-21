6622c55b2fcd1e650729a4b1ffba22dcd1443613
FionaJores <fionajores10@gmail.com>
2024-11-21 22:09:09 +0530

    Updated the resume download link and added Fiona.pdf to public folder

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,9 +9,9 @@ export default function Resume() {
             <div className="md:w-1/2 flex justify-center">
                 <div className="flex flex-col justify-center text-white">
                     <h1 className="text-4xl border-b-4 border-primary mb-5 w-[145px] font-bold">RESUME</h1>
-                    <p className="pb-5 ">
+                    <p className="pb-5">
                         YOU CAN VIEW MY RESUME 
-                        <a href="/Fiona.pdf" className="btn ml-2">Download</a>
+                        <a href="/portfolio/Fiona.pdf" className="btn mt-5 md:mt-0 ml-2">Download</a>
                     </p>
                 </div>
             </div>
